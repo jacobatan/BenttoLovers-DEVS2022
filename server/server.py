c22c8d2ca1381af0c5b087bd5c24bc678145133c
Jacob Tan-WSL <jacob.tan229@gmail.com>
2022-07-09 11:12:59 +1200

    example firebase GET requests

--- server/server.py
+++ server/server.py
@@ -1,10 +1,24 @@
 from flask import Flask
 
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return "Hello World!"
+
+@app.route('/difficulty/<id>')
+def diff(id):
+    data = {"words": ["pain", "hate"]}
+    db.collection("difficulty").document(" ultra hard").set(data)
+    return id
 
 if __name__ == "__main__":
     app.run(debug=True)
