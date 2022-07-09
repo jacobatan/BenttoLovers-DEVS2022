e4d926ebbc30ab5e0fd706edeb73693ee7bdbdb3
Jacob Tan-WSL <jacob.tan229@gmail.com>
2022-07-09 15:00:50 +1200

    routes for file upload

--- server/server.py
+++ server/server.py
@@ -3,9 +3,10 @@ from flask import Flask
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+from firebase_admin import storage
 
 cred = credentials.Certificate("serviceAccountKey.json")
-firebase_admin.initialize_app(cred)
+firebase_admin.initialize_app(cred, {'storageBucket': 'devs-2022.appspot.com'})
 db = firestore.client()
 
 app = Flask(__name__)
@@ -20,6 +21,17 @@ def index():
 def diff(difficulty):
     result = db.collection("difficulty").document(difficulty).get()
     return result.to_dict()
+
+
+@app.route('/file/upload')
+def upload():
+    fileName = "sample.mp3"
+    bucket = storage.bucket()
+    blob = bucket.blob(fileName)
+    blob.upload_from_filename(fileName)
+    blob.make_public()
+    blob.download_to_filename(fileName)
+    return "done!"
 
 
 if __name__ == "__main__":
