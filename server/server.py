1b421c3817bbc374da0dd8129b1f1262eb0fd766
Jacob Alexander Tan <57165244+jacobatan@users.noreply.github.com>
2022-07-09 18:13:58 +1200

    Get difficulty (#6)
    * feat: GET req to diff
    
    * feat: added methods array for difficulty route
    
    * Get Score implementation
    
    * routes for file upload
    
    * feat: able to upload .mp3 + added to users db
    
    * chore/refactor: added comments and fixed line spacing
    
    Co-authored-by: Aditoi2012 <adityasoh@gmail.com>
    Co-authored-by: Aditoi2012 <63826407+Aditoi2012@users.noreply.github.com>

--- server/server.py
+++ server/server.py
@@ -1,24 +1,65 @@
-from flask import Flask
+from flask import Flask, request
 
 import firebase_admin
+import itertools
 from firebase_admin import credentials
 from firebase_admin import firestore
+from firebase_admin import storage
+from uuid import uuid4
+
 
 cred = credentials.Certificate("serviceAccountKey.json")
-firebase_admin.initialize_app(cred)
+firebase_admin.initialize_app(cred, {'storageBucket': 'devs-2022.appspot.com'})
 db = firestore.client()
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return "Hello World!"
 
-@app.route('/difficulty/<id>')
-def diff(id):
-    data = {"words": ["pain", "hate"]}
-    db.collection("difficulty").document(" ultra hard").set(data)
-    return id
+
+@app.route('/<difficulty>', methods=['GET'])
+def diff(difficulty):
+    result = db.collection("difficulty").document(difficulty).get()
+    return result.to_dict()
+
+
+@app.route('/file/upload/<id>')
+def upload(id):
+    # the name of the file on the site + magic
+    fileName = "sample.mp3"
+    bucket = storage.bucket()
+    blob = bucket.blob(fileName)
+
+    # custom token
+    token = uuid4()
+    metadata = {"firebaseStorageDownloadTokens": token}
+    blob.metadata = metadata
+
+    # uploading and making the file public
+    blob.upload_from_filename(fileName)
+    blob.make_public()
+
+    # link to the mp3 file
+    link = "https://firebasestorage.googleapis.com/v0/b/devs-2022.appspot.com/o/{}?alt=media&token={}".format(
+        fileName, token)
+    # print(blob.public_url)
+
+    # handles updating the users video links
+    result = db.collection('user').document(id).get().to_dict()
+    videoLinks = result['videoLinks']
+    videoLinks.append(link)
+    db.collection('user').document(id).update({"videoLinks": videoLinks})
+    return result
+
+
+@app.route('/<id>/score', methods = ['GET', 'POST'])
+def score(id):
+    resultSnap = db.collection('user').document(id).get()
+    result = resultSnap.to_dict()
+    return result
 
 if __name__ == "__main__":
     app.run(debug=True)
